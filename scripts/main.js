119443514d0f6c60bde662e56d4bf0dafc440148
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2019-09-25 10:00:26 +0900

    fix corsor over the text

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
 'use strict'
 const details = document.getElementsByTagName('details')[0]
-const nav = document.getElementsByTagName`nav`[0]
 let flag
-details.addEventListener('mouseenter', () => {flag = true})
 details.addEventListener('mouseleave', () => {flag = false})
-nav.addEventListener('mouseenter', () => {flag = true})
-nav.addEventListener('mouseleave', () => {flag = false})
+details.addEventListener('mouseenter', () => {flag = true})
 document.addEventListener('mousedown', () => {
   if (!flag) details.removeAttribute('open')
 })
