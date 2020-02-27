4e20b9f13597be77a2e940bd60dbd86e1c067af7
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-02-28 06:09:58 +0900

    fix rectangle collision detect

--- games/platformer/scripts/main.js
+++ games/platformer/scripts/main.js
@@ -16,11 +16,11 @@ let ground = [{
   x: canvas.offsetWidth - size, y: 0, w: size, h: canvas.offsetHeight
 }]
 const moveAcceleration = 1
-let moveConstant = 5
-const normalConstant = 5
+let moveConstant = 1
+const normalConstant = 1
 const dashConstant = 10
 const stopConstant = .1
-const brakeConstant = .9
+const brakeConstant = .1
 const gravityConstant = 1
 const jumpConstant = size
 let jumpFlag = false
@@ -82,64 +82,94 @@ const input = () => {
   if (key.d) {
     if (ownCondition.dx < moveConstant) ownCondition.dx += moveAcceleration
   }
-  if (key.j) {
-      jumpTime += 1
-    if (!jumpFlag) {
-      ownCondition.dy -= jumpConstant
-      jumpFlag = true
-    }
-  } else {
-    if (jumpConstant < jumpTime) jumpTime = 0
-    if (jumpTime !== 0) ownCondition.dy += jumpConstant - jumpTime
-    jumpTime = 0
-  }
-  if (key.k) {
-    moveConstant =  dashConstant
-    if (-stopConstant < ownCondition.dx && ownCondition.dx < stopConstant) {
-      ownCondition.dx = ownCondition.dx / 2
-    }
-  } else moveConstant = normalConstant
-  if (key.s) {
-    if (jumpChargeTime < jumpConstant && !jumpFlag) jumpChargeTime += 1
-  } else if (jumpChargeTime !== 0) {
-    ownCondition.dy -= jumpChargeTime
-    jumpChargeTime = 0
-    jumpFlag = true
-  }
-  if (key.w) {
-    if (!jumpFlag) {
-      ownCondition.dy -= jumpConstant
-      jumpFlag = true
-    }
-  }
+  if (key.w) ownCondition.dy -= moveAcceleration // temporary
+  if (key.s) ownCondition.dy += moveAcceleration // temporary
+  // if (key.j) {
+  //     jumpTime += 1
+  //   if (!jumpFlag) {
+  //     ownCondition.dy -= jumpConstant
+  //     jumpFlag = true
+  //   }
+  // } else {
+  //   if (jumpConstant < jumpTime) jumpTime = 0
+  //   if (jumpTime !== 0) ownCondition.dy += jumpConstant - jumpTime
+  //   jumpTime = 0
+  // }
+  // if (key.k) {
+  //   moveConstant =  dashConstant
+  //   if (-stopConstant < ownCondition.dx && ownCondition.dx < stopConstant) {
+  //     ownCondition.dx = ownCondition.dx / 2
+  //   }
+  // } else moveConstant = normalConstant
+  // if (key.s) {
+  //   if (jumpChargeTime < jumpConstant && !jumpFlag) jumpChargeTime += 1
+  // } else if (jumpChargeTime !== 0) {
+  //   ownCondition.dy -= jumpChargeTime
+  //   jumpChargeTime = 0
+  //   jumpFlag = true
+  // }
+  // if (key.w) {
+  //   if (!jumpFlag) {
+  //     ownCondition.dy -= jumpConstant
+  //     jumpFlag = true
+  //   }
+  // }
 }
 const collisionDetect = () => {
   ownCondition.x += ownCondition.dx
   ownCondition.y += ownCondition.dy
-  ownCondition.dy += gravityConstant
-  if (size < ownCondition.dy) ownCondition.dy = size // terminal speed
-  ground.forEach(obj => {
-    if (
-      obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
-      obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
-    ) {
-      if (ownCondition.x < obj.x) console.log('hello')
-      if (ownCondition.y < obj.y + size) {
-        ownCondition.y = obj.y - size
-        jumpFlag = false
-        ownCondition.dx = ownCondition.dx * brakeConstant
-        ownCondition.dy = 0
-      } else if (obj.y + obj.h - size < ownCondition.y) {
-        ownCondition.y = obj.y+ obj.h + size
-        ownCondition.dy = -ownCondition.dy
+  ownCondition.dx = 0
+  ownCondition.dy = 0
+  // ownCondition.dy += gravityConstant
+  // if (size < ownCondition.dy) ownCondition.dy = size // terminal speed
+  // ground.forEach(obj => {
+  //   if (
+  //     obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
+  //     obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
+  //   ) {
+  //     if (ownCondition.x < obj.x) console.log('hello')
+  //     if (ownCondition.y < obj.y + size) {
+  //       ownCondition.y = obj.y - size
+  //       jumpFlag = false
+  //       ownCondition.dx = ownCondition.dx * brakeConstant
+  //       ownCondition.dy = 0
+  //     } else if (obj.y + obj.h - size < ownCondition.y) {
+  //       ownCondition.y = obj.y+ obj.h + size
+  //       ownCondition.dy = -ownCondition.dy
+  //     }
+  //   }
+  //   if (
+  //     obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
+  //     obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
+  //   ) {
+  //     ownCondition.x -= ownCondition.dx
+  //     ownCondition.dx = -ownCondition.dx
+  //   }
+  // })
+  terrainList.forEach((y, iY) => {
+    for (let iX = 0; iX < terrainList[0].length; iX++) {
+      if (y[iX] === '1') {
+        // 矩形 * 円の当たり判定
+        const ox = ownCondition.x // 自機のx座標
+        const oy = ownCondition.y // 自機のy座標
+        const lt = iX * size // 左
+        const rt = iX * size + size // 右
+        const up = iY * size // 上
+        const dn = iY * size + size // 下
+        const rs = size // 自機の半径
+        if (
+          up - rs < oy && oy < dn + rs && // 1 縦
+          lt - rs < ox && ox < rt + rs // 2 横
+        ) {
+          console.log('detect')
+        }
+        // 3 四隅
+        // 左上
+        // 右上
+        // 右下
+        // 左下
+
       }
-    }
-    if (
-      obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
-      obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
-    ) {
-      ownCondition.x -= ownCondition.dx
-      ownCondition.dx = -ownCondition.dx
     }
   })
 }
@@ -154,11 +184,6 @@ const draw = () => {
   context.arc(ownCondition.x, ownCondition.y + jumpChargeTime, size, 0, Math.PI * 2, false)
   context.fillStyle = context.strokeStyle = 'hsl(30, 100%, 60%)'
   context.fill()
-  const drawGround = () => {
-    context.fillStyle = 'hsl(180, 100%, 50%)'
-    ground.forEach(obj => context.fillRect(obj.x, obj.y, obj.w, obj.h))
-  }
-  drawGround()
   context.fillStyle = 'hsl(0, 100%, 50%)'
   context.fillRect(ownCondition.x, ownCondition.y, size, size)
 }
