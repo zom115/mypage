d35b2ecc30532a661ca709ab245b44b44a8ff650
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-02-28 18:42:32 +0900

    add collision detect to side wall

--- games/platformer/scripts/main.js
+++ games/platformer/scripts/main.js
@@ -5,16 +5,11 @@ const size = 16
 // y: terrainList.length,
 // x: terrainList[0].length
 let ownCondition = {
-  x: canvas.offsetWidth * 1 / 8, y: canvas.offsetHeight * 7 / 8,
-  dx: 0, dy: 0
+  x: canvas.offsetWidth * 1 / 8,
+  y: canvas.offsetHeight * 7 / 8,
+  dx: 0,
+  dy: 0
 }
-let ground = [{
-  x: 0, y: canvas.offsetHeight * 31 / 32, w: canvas.offsetWidth, h: size
-}, {
-  x: 0, y: 0, w: size, h: canvas.offsetHeight
-}, {
-  x: canvas.offsetWidth - size, y: 0, w: size, h: canvas.offsetHeight
-}]
 const moveAcceleration = 1
 let moveConstant = 1
 const normalConstant = 1
@@ -22,6 +17,7 @@ const dashConstant = 10
 const stopConstant = .1
 const brakeConstant = .1
 const gravityConstant = 1
+const elasticModulus = 0
 const jumpConstant = size
 let jumpFlag = false
 let jumpTime = 0
@@ -48,13 +44,13 @@ const terrainList = [
   '100000000000000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
-  '100011000000000000000000000000000000000000000000000001',
+  '100000000000000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
   '100000000110000000000000000000000000000000000000000001',
-  '100000000110000000000000000000000000000000000000000001',
+  '100010000110000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
-  '100010000000000000000000000000000000000000000000000001',
+  '100000000000000000000000000000000000000000000000000001',
   '100000000000000000000000000000000000000000000000000001',
   '111111111111111111111111111111111111111111111111111111'
 ]
@@ -117,38 +113,12 @@ const input = () => {
 }
 const collisionDetect = () => {
   // ownCondition.dy += gravityConstant
-  // if (size < ownCondition.dy) ownCondition.dy = size // terminal speed
-  // ground.forEach(obj => {
-  //   if (
-  //     obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
-  //     obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
-  //   ) {
-  //     if (ownCondition.x < obj.x) console.log('hello')
-  //     if (ownCondition.y < obj.y + size) {
-  //       ownCondition.y = obj.y - size
-  //       jumpFlag = false
-  //       ownCondition.dx = ownCondition.dx * brakeConstant
-  //       ownCondition.dy = 0
-  //     } else if (obj.y + obj.h - size < ownCondition.y) {
-  //       ownCondition.y = obj.y+ obj.h + size
-  //       ownCondition.dy = -ownCondition.dy
-  //     }
-  //   }
-  //   if (
-  //     obj.x - size < ownCondition.x && ownCondition.x < obj.x + obj.w + size &&
-  //     obj.y - size < ownCondition.y && ownCondition.y < obj.y + obj.h + size
-  //   ) {
-  //     ownCondition.x -= ownCondition.dx
-  //     ownCondition.dx = -ownCondition.dx
-  //   }
-  // })
-  let moveFlag = true
   terrainList.forEach((y, iY) => {
     for (let iX = 0; iX < terrainList[0].length; iX++) {
       if (y[iX] === '1') {
         const r = size
+        const tileWidth = size
         { // floor
-          const tileWidth = size
           const ax = iX * size - r
           const ay = iY * size - r
           const bx = iX * size + tileWidth + r
@@ -169,18 +139,16 @@ const collisionDetect = () => {
             const bcy = cy - by
             const doc = acx * bcx + acy * bcy
             if (doc < 0) {
-              console.log('detect floor', iX)
-              ownCondition.y -= t * ownCondition.dy
-              // ownCondition.dy = 0
+              // console.log('detect floor', iX, t)
+              ownCondition.dy = -ownCondition.dy * elasticModulus
             }
           }
         }
         { // ceil
-          const tileWidth = size
           const ax = iX * size - r
-          const ay = iY * size + size + r
+          const ay = iY * size + tileWidth + r
           const bx = iX * size + tileWidth + r
-          const by = iY * size + size + r
+          const by = iY * size + tileWidth + r
           const abx = bx - ax
           let ny = abx
           let length = (ny ** 2) ** .5
@@ -197,78 +165,76 @@ const collisionDetect = () => {
             const bcy = cy - by
             const doc = acx * bcx + acy * bcy
             if (doc < 0) {
-              console.log('detect ceil', iX)
-              ownCondition.y -= t * ownCondition.dy
-              // ownCondition.dy = 0
+              // console.log('detect ceil', iX)
+              ownCondition.dy = -ownCondition.dy
             }
           }
         }
-      // if (y[iX] === '1') {
-      //   const r = size
-      //   const tileWidth = size
-      //   const ax = iX * size - r
-      //   const ay = iY * size
-      //   const bx = iX * size + tileWidth + r
-      //   const by = iY * size
-      //   const abx = bx - ax
-      //   const aby = by - ay
-      //   let nx = -aby
-      //   let ny = abx
-      //   let length = (nx ** 2 + ny ** 2) ** .5
-      //   if (0 < length) length = 1 / length
-      //   nx *= length
-      //   ny *= length
-      //   const d = -(ax * nx + ay * ny)
-      //   const t = -(nx * ownCondition.x + ny * ownCondition.y + d) / (nx * ownCondition.dx + ny * ownCondition.dy)
-      //   if (0 < t && t <= 1) {
-      //     const cx = ownCondition.x + ownCondition.dx * t
-      //     const cy = ownCondition.y + ownCondition.dy * t
-      //     const acx = cx - ax
-      //     const acy = cy - ay
-      //     const bcx = cx - bx
-      //     const bcy = cy - by
-      //     const doc = acx * bcx + acy * bcy
-      //     if (doc < 0) {
-      //       console.log('detect', iX)
-      //       ownCondition.y -= t * ownCondition.dy
-      //       // ownCondition.dy = 0
-      //     }
-      //   }
-
-        // // 矩形 * 円の当たり判定
-        // const ox = ownCondition.x // 自機のx座標
-        // const oy = ownCondition.y // 自機のy座標
-        // const lt = iX * size // 左
-        // const rt = iX * size + size // 右
-        // const tp = iY * size // 上
-        // const bm = iY * size + size // 下
-        // const rs = size // 自機の半径
-        // if ( // 矩形
-        //   tp - rs < oy && oy < bm + rs && // 1 縦
-        //   lt - rs < ox && ox < rt + rs && // 2 横
-        //   ( // 3 四隅 a.k.a. 点と矩形
-        //     (lt - ox) ** 2 + (tp - oy) ** 2 < rs ** 2 || // 左上
-        //     (rt - ox) ** 2 + (tp - oy) ** 2 < rs ** 2 || // 右上
-        //     (rt - ox) ** 2 + (bm - oy) ** 2 < rs ** 2 || // 右下
-        //     (lt - ox) ** 2 + (bm - oy) ** 2 < rs ** 2 // 左下
-        //   )
-        // ) {
-        //   console.log('detect')
-        //   if (moveFlag) {
-        //     const dx = ownCondition.dx
-        //     if (0 !== dx) ownCondition.x -= dx
-        //     const dy = ownCondition.dy
-        //     if (0 !== dy) ownCondition.y -= dy
-        //     moveFlag = false
-        //   }
-        // }
+        { // left wall
+          const ax = iX * size - r
+          const ay = iY * size - r
+          const bx = iX * size - r
+          const by = iY * size + tileWidth + r
+          const abx = bx - ax
+          const aby = by - ay
+          let nx = -aby
+          let ny = abx
+          let length = (nx ** 2 + ny ** 2) ** .5
+          if (0 < length) length = 1 / length
+          nx *= length
+          ny *= length
+          const d = -(ax * nx + ay * ny)
+          const t = -(nx * ownCondition.x + ny * ownCondition.y + d) / (
+            nx * ownCondition.dx + ny * ownCondition.dy)
+          if (0 < t && t <= 1) {
+            const cx = ownCondition.x + ownCondition.dx * t
+            const cy = ownCondition.y + ownCondition.dy * t
+            const acx = cx - ax
+            const acy = cy - ay
+            const bcx = cx - bx
+            const bcy = cy - by
+            const doc = acx * bcx + acy * bcy
+            if (doc < 0) {
+              console.log('detect left', iX)
+              ownCondition.dx = -ownCondition.dx
+            }
+          }
+        }
+        { // right wall
+          const ax = iX * size + tileWidth + r
+          const ay = iY * size - r
+          const bx = iX * size + tileWidth + r
+          const by = iY * size + tileWidth + r
+          const abx = bx - ax
+          const aby = by - ay
+          let nx = -aby
+          let ny = abx
+          let length = (nx ** 2 + ny ** 2) ** .5
+          if (0 < length) length = 1 / length
+          nx *= length
+          ny *= length
+          const d = -(ax * nx + ay * ny)
+          const t = -(nx * ownCondition.x + ny * ownCondition.y + d) / (
+            nx * ownCondition.dx + ny * ownCondition.dy)
+          if (0 < t && t <= 1) {
+            const cx = ownCondition.x + ownCondition.dx * t
+            const cy = ownCondition.y + ownCondition.dy * t
+            const acx = cx - ax
+            const acy = cy - ay
+            const bcx = cx - bx
+            const bcy = cy - by
+            const doc = acx * bcx + acy * bcy
+            if (doc < 0) {
+              console.log('detect left', iX)
+              ownCondition.dx = -ownCondition.dx
+            }
+          }
+        }
       }
     }
   })
-  if (moveFlag) {
-    ownCondition.x += ownCondition.dx
-    ownCondition.y += ownCondition.dy
-  }
+  ownCondition.x += ownCondition.dx
+  ownCondition.y += ownCondition.dy
   ownCondition.dx = 0
   ownCondition.dy = 0
 }
