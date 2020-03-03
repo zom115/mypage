a1c487928c019ed081b912b121844429c5b8ac40
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-03-03 11:21:54 +0900

    add many canvas

--- games/dungeon/scripts/main.js
+++ games/dungeon/scripts/main.js
@@ -1,25 +1,39 @@
 {'use strict'
 const display = document.getElementById`display`
-const canvas = document.createElement`canvas`
 const size = 16
-canvas.width = size * 16 * 3
-canvas.height = size * 9
-const context = canvas.getContext`2d`
-display.appendChild(canvas)
 let gX = 0
 let gY = 0
-document.addEventListener('mousemove', e => {
+let canvasId = 0
+const pushCanvas = () => {
+  const canvas = document.createElement`canvas`
+  canvas.id = canvasId
+  canvas.width = size * 16 * 3
+  canvas.height = size * 9
+  const context = canvas.getContext`2d`
+  // const rect = document.getElementById(canvas.Id).getBoundingClientRect()
+  display.appendChild(canvas)
   const rect = canvas.getBoundingClientRect()
-  gX = e.clientX - rect.left
-  gY = e.clientY - rect.top
+  canvasId++
+  canvas.addEventListener('mousemove', e => {
+    gX = e.clientX - rect.left
+    gY = e.clientY - rect.top
+    drawIndicator(canvas, context)
+  }, false)
+}
+const button = document.createElement`button`
+button.textContent = 'Push Canvas'
+button.addEventListener('click', () => {
+  pushCanvas()
 }, false)
-const drawIndicator = () => {
-  context.fillText('test', size * 2, size * 2)
-  context.fillText(`${gX} ${gY}`, size * 2, size * 3)
+display.appendChild(button)
+// pushCanvas()
+const drawIndicator = (canvas, context) => {
+  context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
+  context.fillText(`id: ${canvas.id}`, size * 2, size * 2)
+  context.fillText(`x: ${gX}`, size * 2, size * 3)
+  context.fillText(`y: ${gY}`, size * 2, size * 4)
 }
 const main = () => {
-  context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
-  drawIndicator()
   window.requestAnimationFrame(main)
 }
 main()
