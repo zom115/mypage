c697c435ded75e6d0872ef4357bcee2d0af7dc6f
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-03-04 15:11:40 +0900

    add table

--- games/dungeon/scripts/main.js
+++ games/dungeon/scripts/main.js
@@ -2,6 +2,33 @@
 const display = document.getElementById`display`
 const size = 16
 let canvasId = 0
+const commodities = document.createElement`table`
+const tr = document.createElement`tr`
+const th = document.createElement`th`
+th.textContent = 'Commodities'
+const value = document.createElement`th`
+value.textContent = 'value'
+display.appendChild(commodities)
+commodities.appendChild(tr)
+tr.appendChild(th)
+tr.appendChild(value)
+const generateTableColumn = (d, v) => {
+  const tr = document.createElement`tr`
+  const item = document.createElement`td`
+  const value = document.createElement`td`
+  item.textContent = d
+  value.textContent = v
+  value.className = 'value'
+  commodities.appendChild(tr)
+  tr.appendChild(item)
+  tr.appendChild(value)
+}
+comoditiesList = []
+comoditiesNameList = ['Grain', 'Livestock', 'Fruit', 'Wool', 'Timber']
+comoditiesNameList.forEach(v => {
+  comoditiesList.push({name: v, value: 0})
+})
+comoditiesList.forEach(v => generateTableColumn(v.name, v.value))
 const pushCanvas = () => {
   const canvas = document.createElement`canvas`
   canvas.id = canvasId
