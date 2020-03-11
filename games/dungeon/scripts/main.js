7877eb73650044c76ca621db345dab0cd9ce0916
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-03-11 19:53:16 +0900

    fix building plus button judgment

--- games/dungeon/scripts/main.js
+++ games/dungeon/scripts/main.js
@@ -780,9 +780,11 @@ const elementUpdate = () => {
       document.getElementById(`building-minus-${i}`).disabled = true
     } else document.getElementById(`building-minus-${i}`).disabled = false
     if (jobObject['None'] <= workerList.reduce((acc, cur) => {
-      if (cur.post === 'None' && cur.state === 'return') return ++acc
+      if (cur.post === 'None' || cur.state === 'return') return ++acc
       else return acc
-    }, 0)) {
+    }, 0) && Object.entries(recipeList[i].in).every(([k, v]) => {
+      console.log(commoditiesObject[k] , v)
+      return commoditiesObject[k] < v})) {
       document.getElementById(`building-plus-${i}`).disabled = true
     } else document.getElementById(`building-plus-${i}`).disabled = false
     const progress = document.getElementById(`building-progress-${i}`)
