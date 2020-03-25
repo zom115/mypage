c1f433f12f144f5ee522ba022ae80912d7092d23
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-03-26 06:20:24 +0900

    change scripts to modules

--- games/defense/modules/main.mjs
+++ games/defense/modules/main.mjs
@@ -1,5 +1,3 @@
-!(x = x => {'use strict'
-
 const canvas = document.getElementById`canvas`
 const context = canvas.getContext`2d`
 const size = 16
@@ -7,13 +5,13 @@ let score
 let highscore = 0
 let time
 let mouseFlag = false
-let mouseCooldinate = {x: 0, y: 0}
+let mouseCooldinateObject = {x: 0, y: 0}
 const playerRadius = size
-let bullets
+let bulletList
 const bulletRadius = size / 2
 let rapidFireRate
 let shotTimeStamp
-let enemies
+let enemyList
 let enemyAppearanceRate
 const decreaseRate = 5
 const reactionRate = 5
@@ -21,19 +19,19 @@ const burstRate = 1
 let enemyTimeStamp
 
 const enemyRadius = size * 1.5
-let afterglowScore
+let afterglowScoreList
 const displayTime = 60
 const reset = () => {
   if (highscore < score) highscore = score
   score = 0
   time = 0
-  bullets = []
-  rapidFireRate = 60
+  bulletList = []
+  rapidFireRate = 60 * 4
   shotTimeStamp = -rapidFireRate
-  enemies = []
-  enemyAppearanceRate = 240
+  enemyList = []
+  enemyAppearanceRate = 240 * 4
   enemyTimeStamp = -enemyAppearanceRate
-  afterglowScore = []
+  afterglowScoreList = []
 }
 reset()
 canvas.addEventListener('mouseover', () => {
@@ -43,8 +41,8 @@ canvas.addEventListener('mouseover', () => {
 }, false)
 canvas.addEventListener('mousemove', e => {
   let rect = e.target.getBoundingClientRect()
-  mouseCooldinate.x = e.clientX - rect.left
-  mouseCooldinate.y = e.clientY - rect.top
+  mouseCooldinateObject.x = e.clientX - rect.left
+  mouseCooldinateObject.y = e.clientY - rect.top
 }, false)
 const shot = () => {
   shotTimeStamp = time
@@ -52,13 +50,13 @@ const shot = () => {
     rapidFireRate -= burstRate
   }
   const info = {
-    x: canvas.offsetWidth / 2 - mouseCooldinate.x,
-    y: canvas.offsetHeight / 2 - mouseCooldinate.y
+    x: canvas.offsetWidth / 2 - mouseCooldinateObject.x,
+    y: canvas.offsetHeight / 2 - mouseCooldinateObject.y
   }
   info.distance = (info.x**2+info.y**2)**.5
   if (info.distance !== 0) {
     const bulletSpeed = 100
-    bullets.push({
+    bulletList.push({
       x: canvas.offsetWidth / 2, y: canvas.offsetHeight / 2,
       dx: (info.x / info.distance) * (bulletSpeed / info.distance),
       dy: (info.y / info.distance) * (bulletSpeed / info.distance)
@@ -67,20 +65,20 @@ const shot = () => {
 }
 canvas.addEventListener('mousedown', () => {mouseFlag = true}, false)
 canvas.addEventListener('mouseup', () => {mouseFlag = false}, false)
-const internalProcess = () => {
+setInterval(() => {
   time += 1
   if (mouseFlag && shotTimeStamp + rapidFireRate < time) shot()
   const bulletProcess = () => {
-    bullets.forEach((value, index) => {
+    bulletList.forEach((value, index) => {
       value.x -= value.dx
       value.y -= value.dy
       if (
         value.x < 0 || canvas.offsetWidth < value.x ||
         value.y < 0 || canvas.offsetHeight < value.y
       ) {
-        bullets.splice(index, 1)
+        bulletList.splice(index, 1)
         score -= 10
-        afterglowScore.push({
+        afterglowScoreList.push({
           x: canvas.offsetWidth - size * 1.8, y: size * 4.5,
           score: -10, time: displayTime
         })
@@ -94,36 +92,36 @@ const internalProcess = () => {
 
       const r = ((canvas.offsetWidth / 1.8) ** 2 + (canvas.offsetHeight / 1.8) ** 2) ** .5
       const a = ~~(Math.random() * 360 + 1)
-      enemies.push({
+      enemyList.push({
         x: canvas.offsetWidth / 2 + r * Math.cos(a * (Math.PI / 180)),
         y: canvas.offsetHeight / 2 + r * Math.sin(a * (Math.PI / 180)),
         hue: ~~(Math.random() * 360 + 1)
       })
     }
-    enemies.forEach(value => {
+    enemyList.forEach(value => {
     const width = canvas.offsetWidth / 2 - value.x
     const height = canvas.offsetHeight / 2 - value.y
     const length = (width ** 2 + height ** 2) ** .5
-    const enemySpeed = 2.5
+    const enemySpeed = 10
       value.x += (width / length) / enemySpeed
       value.y += (height / length) / enemySpeed
     })
   }
   const collisionDetect = () => {
-    enemies.forEach((enemy, enemyIndex) => {
+    enemyList.forEach((enemy, enemyIndex) => {
       const width = canvas.offsetWidth / 2 - enemy.x
       const height = canvas.offsetHeight / 2 - enemy.y
       const length = (width ** 2 + height ** 2) ** .5
-      bullets.forEach((bullet, bulletIndex) => {
+      bulletList.forEach((bullet, bulletIndex) => {
         const shotWidth = enemy.x - bullet.x
         const shotHeight = enemy.y - bullet.y
         const shotLength = (shotWidth ** 2 + shotHeight ** 2) ** .5
         if (shotLength < bulletRadius + enemyRadius) {
           const thisScore = length/10|0
           score += thisScore
-          afterglowScore.push({x: enemy.x, y: enemy.y, score: thisScore, time: displayTime})
-          enemies.splice(enemyIndex, 1)
-          bullets.splice(bulletIndex, 1)
+          afterglowScoreList.push({x: enemy.x, y: enemy.y, score: thisScore, time: displayTime})
+          enemyList.splice(enemyIndex, 1)
+          bulletList.splice(bulletIndex, 1)
         }
       })
       if (length < playerRadius + enemyRadius) reset()
@@ -132,8 +130,10 @@ const internalProcess = () => {
   bulletProcess()
   enemyProcess()
   collisionDetect()
-}
-const draw = () => {
+}, 0)
+const main = () => {
+  window.requestAnimationFrame(main)
+  context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
   const indicator = () => {
     context.font = `${size}px sans-serif`
     context.fillStyle = 'hsl(0, 100%, 100%)'
@@ -156,7 +156,7 @@ const draw = () => {
     context.fill()
   }
   const drawBullet = () => {
-    bullets.forEach(value => {
+    bulletList.forEach(value => {
       context.beginPath()
       context.arc(value.x, value.y, size / 2, 0, Math.PI * 2, false)
       context.fillStyle = 'hsl(300, 100%, 85%)'
@@ -164,7 +164,7 @@ const draw = () => {
     })
   }
   const drawEnemy = () => {
-    enemies.forEach(value => {
+    enemyList.forEach(value => {
       context.beginPath()
       context.arc(value.x, value.y, enemyRadius, 0, Math.PI * 2, false)
       context.fillStyle = `hsl(${value.hue}, 100%, 85%)`
@@ -173,18 +173,18 @@ const draw = () => {
   }
   const corsor = () => {
     context.beginPath()
-    context.arc(mouseCooldinate.x, mouseCooldinate.y, size / 2, 0, Math.PI * 2, false)
+    context.arc(mouseCooldinateObject.x, mouseCooldinateObject.y, size / 2, 0, Math.PI * 2, false)
     context.fillStyle = 'hsl(60, 100%, 85%)'
     context.fill()
   }
   const drawAfterglow = () => {
-    afterglowScore.forEach((value, index) => {
+    afterglowScoreList.forEach((value, index) => {
       context.font = `${size}px sans-serif`
       context.fillStyle = 'hsl(0, 100%, 100%)'
       context.textAlign = 'center'
       context.fillText(value.score, value.x, value.y)
       value.time -= 1
-      if (value.time === 0) afterglowScore.splice(index, 1)
+      if (value.time === 0) afterglowScoreList.splice(index, 1)
     })
   }
   indicator()
@@ -193,14 +193,6 @@ const draw = () => {
   drawBullet()
   drawEnemy()
   corsor()
-  if (0 < afterglowScore.length) drawAfterglow()
-}
-const main = () => {
-  internalProcess()
-  context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
-  draw()
-  window.requestAnimationFrame(main)
+  if (0 < afterglowScoreList.length) drawAfterglow()
 }
 main()
-
-})()
