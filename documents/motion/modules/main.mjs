ea09e08d03469073b570e742b6bc53ef2825040c
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2020-04-06 23:17:16 +0900

    add walk under the waist

--- documents/motion/modules/main.mjs
+++ documents/motion/modules/main.mjs
@@ -1,4 +1,14 @@
-import {key} from '../../../modules/key.mjs'
+import {key, globalTimestamp} from '../../../modules/key.mjs'
+const mainFrameList = []
+const animFrameList = []
+const frameCounter = list => {
+  list.push(globalTimestamp)
+  let flag = true
+  do {
+    if (list[0] + 1e3 < globalTimestamp) list.shift()
+    else flag = false
+  } while (flag)
+}
 const canvas = document.getElementById`canvas`
 canvas.width = 256
 canvas.height = 256
@@ -6,15 +16,13 @@ const ctx = canvas.getContext`2d`
 const PI = Math.PI
 const inseam = 17
 const body = {
-  footL:     {from:    'waist', reach:  9, degree:  .25 * PI, default: .25 * PI, x: 50, y: 50},
-  footR:     {from:    'waist', reach:  9, degree:  .75 * PI, default: .75 * PI, x: 50, y: 50},
+  kneeL:     {from:    'waist', reach:  9, degree:  .4 * PI, default: .25 * PI, x: 50, y: 50},
+  kneeR:     {from:    'waist', reach:  9, degree:  .6 * PI, default: .75 * PI, x: 50, y: 50},
+  heelL:     {from:     'kneeL', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
+  heelR:     {from:     'kneeR', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
   waist:     {from:    'bottom', reach:  0, degree:        0, default:        0, x: 50, y: 50},
   // hipL:      {from:     'waist', reach:  3, degree:        0, default:        0, x:  0, y:  0},
   // hipR:      {from:     'waist', reach:  3, degree:       PI, default:       PI, x:  0, y:  0},
-  // kneeL:     {from:      'hipL', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
-  // kneeR:     {from:      'hipR', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
-  // heelL:     {from:     'kneeL', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
-  // heelR:     {from:     'kneeR', reach:  8, degree:  .5 * PI, default:  .5 * PI, x:  0, y:  0},
   // footL:     {from:     'heelL', reach:  2, degree:        0, default:        0, x:  0, y:  0},
   // footR:     {from:     'heelR', reach:  2, degree:       PI, default:       PI, x:  0, y:  0},
   // neck:      {from:     'waist', reach: 11, degree: -.5 * PI, default: -.5 * PI, x:  0, y:  0},
@@ -30,8 +38,8 @@ const range = {
   // head: 6,
   // handL: 2,
   // handR: 2,
-  footL: 2,
-  footR: 2,
+  // footL: 2,
+  // footR: 2,
 }
 Object.values(body).forEach((v) => {
   if (v.from !== 'bottom') {
@@ -40,18 +48,37 @@ Object.values(body).forEach((v) => {
   }
 })
 let isPivotLeft = false
+const diffHistory = {
+  pivotFlag: [],
+  heel: [],
+}
 const main = () => setInterval(() => {
-  const stride = 13
-  const diff = Math.abs(body.footL.x - body.footR.x)
+  frameCounter(mainFrameList)
+
+  const stride = 10
+  const diff = Math.abs(body.kneeL.x - body.kneeR.x)
   if (stride < diff) isPivotLeft = !isPivotLeft
-  const moveConstant = .2
-  if (key.s.flag) isPivotLeft ? body.footL.x -= moveConstant : body.footR.x -= moveConstant
-  if (key.f.flag) isPivotLeft ? body.footL.x += moveConstant : body.footR.x += moveConstant
+  let moveConstant = .2
+  if (key.j.flag) moveConstant *= 2
+  if ((key.s.flag && key.f.flag) || (!key.s.flag && !key.f.flag)) moveConstant = 0
+  else if (key.s.flag) moveConstant = -moveConstant
+  // else if (key.f.flag)
+  // isPivotLeft ? body.kneeL.x -= moveConstant : body.kneeR.x -= moveConstant
+  isPivotLeft ? body.kneeL.x += moveConstant : body.kneeR.x += moveConstant
+  diffHistory.pivotFlag.push(isPivotLeft)
+  diffHistory.heel.push(moveConstant)
   const gravity = .1
+  const length = 10
+  if (length < diffHistory.heel.length) {
+    diffHistory.pivotFlag[0] ? body.heelL.x += diffHistory.heel[0] :
+    body.heelR.x += diffHistory.heel[0]
+    diffHistory.pivotFlag.shift()
+    diffHistory.heel.shift()
+  }
   Object.values(body).forEach((v) => {
     if (v.from === 'bottom') {
       if (v.y <= canvas.offsetWidth - inseam) v.y += gravity
-      v.x = (body.footL.x + body.footR.x) / 2
+      v.x = (body.kneeL.x + body.kneeR.x) / 2
       return
     } else {
       // v.x = body[v.from].x + v.reach * Math.cos(v.degree)
@@ -61,6 +88,7 @@ const main = () => setInterval(() => {
 }, 0)
 const draw = () => {
   window.requestAnimationFrame(draw)
+  frameCounter(animFrameList)
   ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
   ctx.beginPath()
   Object.values(body).forEach(v => {
@@ -75,6 +103,8 @@ const draw = () => {
     ctx.arc(body[k].x, body[k].y, v, 0, PI * 2)
     ctx.fill()
   })
+  ctx.fillText(`internal FPS: ${mainFrameList.length - 1}`, 10, 10)
+  ctx.fillText(`screen FPS: ${animFrameList.length - 1}`, 10, 20)
 }
 {
   main()
