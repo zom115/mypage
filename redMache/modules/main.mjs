9d54a54a3ecb8dbac830b74d5b6ae101fa90cf55
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2021-01-08 07:06:36 +0900

    add link

---
+++ redMache/modules/main.mjs
@@ -0,0 +1,10 @@
+console.log('hello')
+
+// ステージ配置
+// height: 11 + 下段 1 + 上段にせりあがるとgame over
+// width: 本家は3 <= w <= 8, 2はあってもいいと思う
+// n types * m pairs
+// type
+// pair
+// max height
+// max width
