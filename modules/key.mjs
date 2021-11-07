93789e7c22a05b79e25e65c4ab5f5940079304ba
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2021-11-08 01:09:51 +0900

    Add key to list

--- modules/key.mjs
+++ modules/key.mjs
@@ -1,6 +1,12 @@
 const key = {}
+// TODO: Scroll Lock, App Key, etc.
 const keyList = [
-  'Alt', 'Control','Enter', 'Shift', ' ',
+  'Alt', 'Control', 'Escape', 'Tab', 'Enter', 'Shift', 'Backspace', 'Delete',
+  'Insert', 'Home', 'End', 'PageUp', 'PageDown', 'Pause', 'PrintScreen',
+  'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight',
+  'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
+  ' ', '-', '^', '@', '[', ';', ':', ']', ',', '.', '/',
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ]
