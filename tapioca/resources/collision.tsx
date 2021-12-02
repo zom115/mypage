a0f6da422e9a3a5e1be77084a32fe3031100d97c
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2021-12-02 14:17:54 +0900

    Remove file

--- tapioca/resources/collision.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2020.03.18" name="collision" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="../images/maps/collision.png" width="320" height="240"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
-</tileset>
