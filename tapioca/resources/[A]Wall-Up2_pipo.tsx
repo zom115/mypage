e0395b259d6e4675186ec9ac33cce8011b66a59b
zom115 <m1k2h2e3hys4oakmdkz@gmail.com>
2021-12-02 11:01:44 +0900

    Add map data

---
+++ tapioca/resources/[A]Wall-Up2_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2020.03.18" name="[A]Wall-Up2_pipo" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="../images/maps/[A]Wall-Up2_pipo.png" width="256" height="192"/>
+</tileset>
---
+++ tapioca/resources/[Base]BaseChip_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2020.03.18" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1992" columns="8">
+ <image source="../images/maps/[Base]BaseChip_pipo.png" width="256" height="7968"/>
+</tileset>
